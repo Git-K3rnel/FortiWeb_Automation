e55592f20dd6988c46766170b8608a33afdb5797
_K3rnel <127470407+Git-K3rnel@users.noreply.github.com>
2024-01-05 18:28:54 +0330

    Update Check_Allowed_Hosts.py

--- WAF_Reporter/Check_Allowed_Hosts.py
+++ WAF_Reporter/Check_Allowed_Hosts.py
@@ -68,3 +68,40 @@ def get_api_response(url, cred):
 def get_server_policy_list(cred):
     all_server_policies = get_api_response('/api/v2.0/cmdb/server-policy/policy', cred)
     return all_server_policies
+
+def get_policy_content_route_size(cred, policy_name):
+    content_route_size = 0
+    url = f'/api/v2.0/cmdb/server-policy/policy?mkey={policy_name}'
+    policy_details = get_api_response(url, cred)
+    content_route_size = policy_details['sz_http-content-routing-list']
+    return content_route_size
+
+
+def allowed_policy_content_routes(cred, policy_name):
+    isAllowed = True
+    allowed_policy_content_routes = []
+    url1 = f'/api/v2.0/cmdb/server-policy/policy/http-content-routing-list?mkey={policy_name}'
+    content_route_list = get_api_response(url1, cred)
+    for content_route_entry in content_route_list:
+        content_route_id = content_route_entry['id']
+        url2 = f'/api/v2.0/cmdb/server-policy/policy/http-content-routing-list?mkey={policy_name}&sub_mkey={content_route_id}'
+        content_route_detail = get_api_response(url2, cred)
+        web_protection_profile_name = content_route_detail['web-protection-profile']
+        web_protection_profile_name_encoded = requests.utils.quote(web_protection_profile_name)
+        url3 = f'/api/v2.0/cmdb/waf/web-protection-profile.inline-protection?mkey={web_protection_profile_name_encoded}'
+        web_protection_profile_detail = get_api_response(url3, cred)
+        url_access_policy_name = web_protection_profile_detail['url-access-policy']
+        if url_access_policy_name == 'Deny Zone to Zone':
+            continue
+        else:
+            url4 = f'/api/v2.0/cmdb/waf/url-access.url-access-policy/rule?mkey={url_access_policy_name}'
+            url_access_policy_list = get_api_response(url4, cred)
+            for url_access_rule in url_access_policy_list:
+                if url_access_rule['url-access-rule-name'] == 'deny' or url_access_rule['url-access-rule-name'] == 'Deny' or url_access_rule['url-access-rule-name'] == 'block' or url_access_rule['url-access-rule-name'] == 'Block':
+                    isAllowed = False
+                else:
+                    pass
+            if isAllowed:
+                allowed_policy_content_routes.append(
+                    content_route_entry['content-routing-policy-name'])
+    return allowed_policy_content_routes
